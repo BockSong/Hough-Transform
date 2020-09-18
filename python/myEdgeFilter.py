1f11f9ae0e45777caac7bfc0310d59e363499c7e
BockSong <songrcs@gmail.com>
2020-09-17 22:52:17 -0400

    complete NMS and edge filter

--- python/myEdgeFilter.py
+++ python/myEdgeFilter.py
@@ -10,6 +10,8 @@ from matplotlib import pyplot as plt
 import matplotlib
 
 def myEdgeFilter(img0, sigma):
+    img0 = img0.astype('float32')
+
 	# smooth out the image with the Gaussian kernel
     hsize = 2 * math.ceil(3 * sigma) + 1
     hfilt = Gauss2D((hsize, hsize), sigma)
@@ -24,10 +26,7 @@ def myEdgeFilter(img0, sigma):
 
     # compute edge magnitude image and edge orientation image
     Im = np.sqrt(np.square(Ix) + np.square(Iy))
-    Io = np.arctan(Iy / Ix)
-
-    # perform NMS
-    Img1 = Im
+    Io = np.arctan(Iy / Ix) # gradient directions
 
     '''
     plt.imshow(Ix,cmap="gray")
@@ -36,6 +35,29 @@ def myEdgeFilter(img0, sigma):
     plt.show()
     '''
     
+    Img1 = np.copy(Im)
+
+    # perform NMS
+    dgroups = np.array([0, np.pi / 4, np.pi / 2, (3 / 4) * np.pi, np.pi, - np.pi / 4, - np.pi / 2, - (3 / 4) * np.pi, - np.pi])
+    borderSize = 1 # assume a 3*3 filter for NMS
+
+    for i in range(borderSize, len(Im) - borderSize):
+        for j in range(borderSize, len(Im[0]) - borderSize):
+            idx = np.argmin(np.abs(dgroups - Io[i][j]))
+
+            if idx == 0 or idx == 4 or idx == 9:
+                if (Im[i][j] < Im[i][j - 1] and Im[i][j] < Im[i][j + 1]):
+                    Img1[i][j] = 0
+            elif idx == 2 or idx == 6:
+                if (Im[i][j] < Im[i - 1][j] and Im[i][j] < Im[i + 1][j]):
+                    Img1[i][j] = 0
+            elif idx == 1 or idx == 8:
+                if (Im[i][j] < Im[i - 1][j - 1] and Im[i][j] < Im[i + 1][j + 1]):
+                    Img1[i][j] = 0
+            elif idx == 3 or idx == 5:
+                if (Im[i][j] < Im[i - 1][j + 1] and Im[i][j] < Im[i + 1][j - 1]):
+                    Img1[i][j] = 0
+
     return Img1,Io,Ix,Iy
 
 
